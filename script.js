efadde225668c7746f7a486e6a50880d8ed04d35
Flosch37 <florianschnabel18@gmail.com>
2022-10-17 16:45:31 +0200

    test injection sur bleu, et limites a 2, reste rouge et jaunes

--- script.js
+++ script.js
@@ -4,61 +4,83 @@ function createCardYellow() {
   let cardYellow = document.createElement("div");
   cardYellow.classList.add("card");
   cardYellow.classList.add("cardYellow");
-  return cardYellow
-//   cardContainer.appendChild(cardYellow);
+  return cardYellow;
+  //   cardContainer.appendChild(cardYellow);
 }
 
 function createCardRed() {
   let CardRed = document.createElement("div");
   CardRed.classList.add("card");
   CardRed.classList.add("cardRed");
-  return CardRed
-//   cardContainer.appendChild(CardRed);
+  return CardRed;
+  //   cardContainer.appendChild(CardRed);
 }
 
 function createCardBlue() {
   let cardBlue = document.createElement("div");
   cardBlue.classList.add("card");
   cardBlue.classList.add("cardBlue");
-  return cardBlue
-//   cardContainer.appendChild(cardBlue);
+  return cardBlue;
+  //   cardContainer.appendChild(cardBlue);
 }
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+  return Math.floor(Math.random() * max);
+}
 
 // injecte un nombres de cartes en parametres avec un placement aléatoire
 
-function injectCardContainer(numberCard){
-let arrayCards = []
-if(numberCard == 6){
-    arrayCards.push("Blue")
-    arrayCards.push("Red")
-    arrayCards.push("Blue")
-    arrayCards.push("Red")
-    arrayCards.push("Yellow")
-    arrayCards.push("Yellow")
-    
-}
-console.log(arrayCards)
-let pair = numberCard / 2 
-for(let i = 0; i <numberCard; i++) {
+function injectCardContainer(numberCard) {
+  let arrayCards = [];
+  if (numberCard == 6) {
+    arrayCards.push("Blue");
+    arrayCards.push("Red");
+    arrayCards.push("Blue");
+    arrayCards.push("Red");
+    arrayCards.push("Yellow");
+    arrayCards.push("Yellow");
+  }
 
-    if(arrayCards[getRandomInt(numberCard - i)] == "Blue") {
-        cardContainer.appendChild(createCardBlue()); 
+  let pair = numberCard / 2;
+  let numberOfBlue = 0;
+  let numberOfRed = 0;
+  let numberOfYellow = 0;
+  for (let i = 0; i < arrayCards.length; i++) {
+    let randomInt = getRandomInt(arrayCards.length);
+
+    if (arrayCards[randomInt] == "Blue") {
+
+        if(numberOfBlue != 2) {
+            cardContainer.appendChild(createCardBlue());
+            numberOfBlue++;
+        }
+      if (numberOfBlue == 2 && numberOfRed != 2) {
+        cardContainer.appendChild(createCardRed());
+        numberOfRed++;
+        
+      } else if(numberOfYellow != 2) {
+        cardContainer.appendChild(createCardYellow());
+        numberOfYellow++
+        
+      }
+      
+    } else if (arrayCards[randomInt] == "Red") {
+      cardContainer.appendChild(createCardRed());
+
+
+
+    } else if (arrayCards[randomInt] == "Yellow") {
+      cardContainer.appendChild(createCardYellow());
     }
+    console.log(randomInt);
+    console.log(arrayCards);
+  }
 
-}
-
-
-
-
-    // createCardBlue()
-    // createCardRed()
-    // createCardYellow()
-    // createCardBlue()
-    // createCardRed()
-    // createCardYellow()
+  // createCardBlue()
+  // createCardRed()
+  // createCardYellow()
+  // createCardBlue()
+  // createCardRed()
+  // createCardYellow()
 }
 
 injectCardContainer(6);
