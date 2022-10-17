0202dc700d772482c3d03d40c4a3474f2e7863ce
Flosch37 <florianschnabel18@gmail.com>
2022-10-17 15:55:30 +0200

    random generator en cours

--- script.js
+++ script.js
@@ -4,31 +4,61 @@ function createCardYellow() {
   let cardYellow = document.createElement("div");
   cardYellow.classList.add("card");
   cardYellow.classList.add("cardYellow");
-  cardContainer.appendChild(cardYellow);
+  return cardYellow
+//   cardContainer.appendChild(cardYellow);
 }
 
 function createCardRed() {
   let CardRed = document.createElement("div");
   CardRed.classList.add("card");
   CardRed.classList.add("cardRed");
-  cardContainer.appendChild(CardRed);
+  return CardRed
+//   cardContainer.appendChild(CardRed);
 }
 
 function createCardBlue() {
   let cardBlue = document.createElement("div");
   cardBlue.classList.add("card");
   cardBlue.classList.add("cardBlue");
-  cardContainer.appendChild(cardBlue);
+  return cardBlue
+//   cardContainer.appendChild(cardBlue);
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+// injecte un nombres de cartes en parametres avec un placement aléatoire
+
+function injectCardContainer(numberCard){
+let arrayCards = []
+if(numberCard == 6){
+    arrayCards.push("Blue")
+    arrayCards.push("Red")
+    arrayCards.push("Blue")
+    arrayCards.push("Red")
+    arrayCards.push("Yellow")
+    arrayCards.push("Yellow")
+    
+}
+console.log(arrayCards)
+let pair = numberCard / 2 
+for(let i = 0; i <numberCard; i++) {
+
+    if(arrayCards[getRandomInt(numberCard - i)] == "Blue") {
+        cardContainer.appendChild(createCardBlue()); 
+    }
+
 }
 
-function injectCardContainer(){
-    createCardBlue()
-    createCardRed()
-    createCardYellow()
-    createCardBlue()
-    createCardRed()
-    createCardYellow()
 
+
+
+    // createCardBlue()
+    // createCardRed()
+    // createCardYellow()
+    // createCardBlue()
+    // createCardRed()
+    // createCardYellow()
 }
 
-injectCardContainer();
+injectCardContainer(6);
